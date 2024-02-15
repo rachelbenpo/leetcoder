8241800074e169c714b8d40f0ab039bfba6c3b62
rachelbenpo <4141893@gmail.com>
2024-02-15 19:53:57 +0200

    create cli forms and functions

---
+++ client/clientForms.go
@@ -0,0 +1,390 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/charmbracelet/huh"
+)
+
+// help variables for storing form data
+var (
+	question      = Question{}
+	ans           = Answer{}
+	testCase      = TestCase{}
+	shortQuestion = QuestionShort{}
+
+	id             string
+	testCasesCount string
+	operation      string = "Choose operation"
+	wantTOAnswer   bool   = false
+
+	questionsList  []Question
+	shortQuestions []QuestionShort
+)
+
+// run form for showing all questions and choosing once
+func runGetAllForm() error {
+
+	// get all questions from the server
+	questionsList, err := GetAllQuestions()
+	if err != nil {
+		return err
+	}
+
+	shortQuestions = getShortQuestions(questionsList)
+
+	// show all questions and choose once
+	allQuestionsForm := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[QuestionShort]().
+				Options(huh.NewOptions(shortQuestions...)...).
+				Title("Choose Question").
+				Value(&shortQuestion)),
+	)
+
+	if err = allQuestionsForm.Run(); err != nil {
+		return err
+	}
+	operation = "Show single question"
+	id = fmt.Sprint(shortQuestion.ID)
+	return nil
+}
+
+// run form for get question by id
+func RunGetByIdForm() error {
+
+	// form for enter id
+	var getIDForm = huh.NewForm(huh.NewGroup(
+		huh.NewInput().
+			Title("Question ID").
+			Placeholder("Enter Question ID").
+			Value(&id),
+	))
+	getIDForm.View()
+	err := getIDForm.Run()
+	if err != nil {
+		return err
+	}
+
+	if question, err = GetQuestionById(id); err != nil {
+		return err
+	}
+
+	// form for showing a single question
+	singleQuestionForm := huh.NewForm(
+		huh.NewGroup(
+			huh.NewNote().
+				Title(question.Name).
+				Description(question.Instructions),
+
+			huh.NewConfirm().
+				Affirmative("try to answer").
+				Negative("cancel").
+				Value(&wantTOAnswer),
+		),
+	)
+
+	if err = singleQuestionForm.Run(); err != nil {
+		return err
+	}
+	if wantTOAnswer {
+		operation = "Check Answer"
+	} else {
+		operation = "Choose operation"
+	}
+	return nil
+}
+
+// run form for showing a single question
+func runShowSingleForm() error {
+
+	var err error
+
+	if question, err = GetQuestionById(id); err != nil {
+		return err
+	}
+
+	// form for show a single question
+	singleQuestionForm := huh.NewForm(
+		huh.NewGroup(
+			huh.NewNote().
+				Title(question.Name).
+				Description(question.Instructions),
+
+			huh.NewConfirm().
+				Affirmative("try to answer").
+				Negative("cancel").
+				Value(&wantTOAnswer),
+		),
+	)
+
+	if err = singleQuestionForm.Run(); err != nil {
+		return err
+	}
+	if wantTOAnswer {
+		operation = "Check Answer"
+	} else {
+		operation = "Choose operation"
+	}
+	return nil
+}
+
+// run form for deletion
+func runDeleteForm() error {
+
+	// form for enter id
+	getIDForm := huh.NewForm(huh.NewGroup(
+		huh.NewInput().
+			Title("Question ID").
+			Placeholder("Enter Question ID").
+			Value(&id),
+	))
+
+	if err := getIDForm.Run(); err != nil {
+		return err
+	}
+
+	if err := DeleteQuestion(id); err != nil {
+		return err
+	}
+	return nil
+}
+
+// run form for update a question
+func runUpdateForm() error {
+
+	// form for enter id
+	var getIDForm = huh.NewForm(huh.NewGroup(
+		huh.NewInput().
+			Title("Question ID").
+			Placeholder("Enter Question ID").
+			Value(&id),
+	))
+
+	if err := getIDForm.Run(); err != nil {
+		return err
+	}
+
+	updateQuestionForm := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("Name").
+				Placeholder("Enter Name").
+				Value(&question.Name),
+			huh.NewInput().
+				Title("Instructions").
+				Placeholder("Enter Instructions").
+				Value(&question.Instructions),
+			huh.NewInput().
+				Title("Answer - optional").
+				Placeholder("Enter Answer if you want").
+				Value(&question.Answer),
+			huh.NewInput().
+				Title("num of test cases").
+				Value(&testCasesCount),
+		).Title("fill only the parameters you want to update"),
+	)
+
+	err := updateQuestionForm.Run()
+	if err != nil {
+		return err
+	}
+	if testCasesCount == "" {
+		question.TestCases = nil
+	} else {
+		count, err := strconv.Atoi(testCasesCount)
+		if err != nil {
+			return err
+		}
+
+		//run form for creating test cases
+		for i := 0; i < count; i++ {
+			testCaseForm := huh.NewForm(
+				huh.NewGroup(
+					huh.NewInput().
+						Title("Input").
+						Placeholder("Enter Input").
+						Value(&testCase.Input),
+					huh.NewInput().
+						Title("Output").
+						Placeholder("Enter Output").
+						Value(&testCase.Output),
+				),
+			)
+			if err := testCaseForm.Run(); err != nil {
+				return err
+			}
+			question.TestCases = append(question.TestCases, testCase)
+			testCase = TestCase{Input: "", Output: ""}
+		}
+	}
+
+	// send HTTP request to the server
+	if err := UpdateQuestion(id, question); err != nil {
+		return err
+	}
+	return nil
+}
+
+// run form for creating a new question
+func runCreateQuestionForm() error {
+
+	createQuestionForm := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("Name").
+				Placeholder("Enter Name").
+				Value(&question.Name),
+			huh.NewInput().
+				Title("Instructions").
+				Placeholder("Enter Instructions").
+				Value(&question.Instructions),
+			huh.NewInput().
+				Title("Answer - optional").
+				Placeholder("Enter Answer if you want").
+				Value(&question.Answer),
+			huh.NewInput().
+				Title("num of test cases").
+				Value(&testCasesCount),
+		),
+	)
+
+	err := createQuestionForm.Run()
+	if err != nil {
+		return err
+	}
+
+	count, err := strconv.Atoi(testCasesCount)
+	if err != nil {
+		return err
+	}
+
+	//run form for creating test cases
+	for i := 0; i < count; i++ {
+		testCaseForm := huh.NewForm(
+			huh.NewGroup(
+				huh.NewInput().
+					Title("Input").
+					Placeholder("Enter Input").
+					Value(&testCase.Input),
+				huh.NewInput().
+					Title("Output").
+					Placeholder("Enter Output").
+					Value(&testCase.Output),
+			),
+		)
+		err := testCaseForm.Run()
+		if err != nil {
+			return err
+		}
+		question.TestCases = append(question.TestCases, testCase)
+		testCase = TestCase{Input: "", Output: ""}
+	}
+
+	// send HTTP request to the server
+	_, err = CreateQuestion(question)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// run form for checking an answer
+func runCheckForm() error {
+
+	if id == "" || id == "0" {
+		// run form for enter id
+		getIDForm := huh.NewForm(huh.NewGroup(
+			huh.NewInput().
+				Title("Question ID").
+				Placeholder("Enter Question ID").
+				Value(&id),
+		))
+
+		if err := getIDForm.Run(); err != nil {
+			return err
+		}
+	}
+
+	checkAnswerForm := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Options(huh.NewOption("javascript", "javascript"), huh.NewOption("python", "python")).
+				Title("Language").
+				Value(&ans.Lang),
+			huh.NewText().
+				Title("Code").
+				Placeholder("Enter Code").
+				Value(&ans.Code),
+		),
+	)
+
+	if err := checkAnswerForm.Run(); err != nil {
+		return err
+	}
+
+	// send question to server for testing
+	correct, err := CheckAnswer(id, ans)
+	if err != nil {
+		return err
+	}
+
+	// check response
+	if correct == "true" {
+		fmt.Println("correct answer.")
+		operation = "Choose operation"
+	} else {
+		fmt.Println("wrong answer.")
+		fmt.Println("message: ", correct)
+		operation = "Check Answer"
+		ans = Answer{Lang: "", Code: ""}
+	}
+	return nil
+}
+
+// run form for choosing which operation to perform
+func chooseOperation() error {
+
+	initVariables()
+
+	operationForm := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Options(huh.NewOptions("Get All Questions", "Get Question by ID", "Create Question", "Update Question", "Delete Question", "Check Answer", "Exit")...).
+				Title("Choose Operation").
+				Value(&operation),
+		),
+	)
+	if err := operationForm.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// init help variables
+func initVariables() {
+
+	question = Question{}
+	ans = Answer{}
+	testCase = TestCase{}
+	shortQuestion = QuestionShort{}
+
+	id = ""
+	testCasesCount = ""
+
+	wantTOAnswer = false
+
+	questionsList = []Question{}
+	shortQuestions = []QuestionShort{}
+}
+
+// get array of short questions
+func getShortQuestions(questions []Question) []QuestionShort {
+
+	var shortQuestions []QuestionShort
+	for _, q := range questions {
+		shortQuestions = append(shortQuestions, QuestionShort{ID: q.ID, Name: q.Name})
+	}
+	return shortQuestions
+}
---
+++ client/clientFunctions.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+)
+
+const baseURL = "http://localhost:8080"
+
+func GetAllQuestions() ([]Question, error) {
+
+	// send HTTP request to server
+	url := baseURL + "/questions"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error making GET request:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("ERROR: status code: %d message: %d", resp.StatusCode, resp.Body)
+	}
+
+	// convert the response body to questions array
+	var questions []Question
+	err = json.NewDecoder(resp.Body).Decode(&questions)
+	if err != nil {
+		fmt.Println("Error decoding response:", err)
+		return nil, err
+	}
+
+	return questions, nil
+}
+
+func GetQuestionById(id string) (Question, error) {
+
+	// send HTTP request
+	url := baseURL + "/questions/" + id
+	resp, err := http.Get(url)
+	if err != nil {
+		return Question{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return Question{}, fmt.Errorf("server returned error. \nstatus code: %d \nmessage: %d", resp.StatusCode, resp.Body)
+	}
+
+	// convert response body to question
+	var question Question
+	err = json.NewDecoder(resp.Body).Decode(&question)
+	if err != nil {
+		return Question{}, fmt.Errorf("error decoding response body: %s", err)
+	}
+
+	return question, nil
+}
+
+func CreateQuestion(question Question) (int, error) {
+
+	url := baseURL + "/questions"
+
+	// convert input question to json
+	jsonData, err := json.Marshal(question)
+	if err != nil {
+		return 0, fmt.Errorf("error marshalling JSON:", err)
+	}
+
+	// send HTTP request
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	// check response status
+	if resp.StatusCode != http.StatusCreated {
+		return 0, fmt.Errorf("server returned error. \nstatus code: %d \nmessage: %d", resp.StatusCode, resp.Body)
+	}
+
+	// extract data from response
+	var id int
+	err = json.NewDecoder(resp.Body).Decode(&id)
+	if err != nil {
+		return 0, fmt.Errorf("Error decoding response: ", err)
+	}
+
+	return id, nil
+}
+
+func UpdateQuestion(id string, question Question) error {
+
+	// send HTTP request
+	url := baseURL + "/questions/" + id
+	jsonData, err := json.Marshal(question)
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON: ", err)
+	}
+
+	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return fmt.Errorf("Error creating request: ", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("Error making PUT request: ", err)
+	}
+	defer resp.Body.Close()
+
+	// check response status code
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("server returned error. \nstatus code: %d \nmessage: %d", resp.StatusCode, resp.Body)
+	}
+
+	return nil
+}
+
+func DeleteQuestion(id string) error {
+
+	url := baseURL + "/questions/" + id
+
+	// send HTTP request
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making DELETE request:", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	// check response status
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: Unexpected status code", resp.StatusCode)
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+func CheckAnswer(id string, ans Answer) (string, error) {
+
+	url := baseURL + "/questions/check-answer/" + id
+
+	// convert input answer to json
+	jsonData, err := json.Marshal(ans)
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return "", err
+	}
+
+	// send HTTP request
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error making POST request:", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: Unexpected status code", resp.StatusCode)
+		return "", fmt.Errorf("error: %d", resp.StatusCode)
+	}
+
+	// get data from response body
+
+	// var isCorrect struct {
+	// 	correct string
+	// }
+
+	is := map[string]string{}
+
+	err = json.NewDecoder(resp.Body).Decode(&is)
+	if err != nil {
+		fmt.Println("Errorrrr decoding response:", err)
+		return "", err
+	}
+
+	return is["correct"], nil
+}
