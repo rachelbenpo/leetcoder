c1ac8f301560b34da8acf9c15553b36d4f6cf497
rachelbenpo <4141893@gmail.com>
2024-02-16 01:29:49 +0200

    fix client

--- client/clientForms.go
+++ client/clientForms.go
@@ -23,6 +23,69 @@ var (
 	shortQuestions []QuestionShort
 )
 
+func RunCliForms() {
+	var err error
+	// run the CLI
+	for {
+		switch operation {
+
+		case "Choose operation":
+			if err = chooseOperation(); err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+
+		case "Get All Questions":
+			if err = runGetAllForm(); err != nil {
+				fmt.Println("error getting all questions from server: ", err)
+				operation = "Choose operation"
+			}
+
+		case "Get Question by ID":
+			if err = RunGetByIdForm(); err != nil {
+				fmt.Println("error getting question from server: ", err)
+				operation = "Choose operation"
+			}
+
+		case "Show single question":
+			if err = runShowSingleForm(); err != nil {
+				fmt.Println("error getting question from server: ", err)
+				operation = "Choose operation"
+			}
+
+		case "Create Question":
+			if err = runCreateQuestionForm(); err != nil {
+				fmt.Println("error creating question: ", err)
+			}
+			operation = "Choose operation"
+
+		case "Update Question":
+			if err := runUpdateForm(); err != nil {
+				fmt.Println("error updating question: ", err)
+			}
+			operation = "Choose operation"
+
+		case "Delete Question":
+			if err = runDeleteForm(); err != nil {
+				fmt.Println("error deleting question:", err)
+			}
+			operation = "Choose operation"
+
+		case "Check Answer":
+			if err = runCheckForm(); err != nil {
+				operation = "Choose operation"
+				fmt.Println("error testing your answer: ", err)
+			}
+		case "Exit":
+			return
+
+		default:
+			fmt.Println("Invalid operation selected.")
+			return
+		}
+	}
+}
+
 // run form for showing all questions and choosing once
 func runGetAllForm() error {
 
--- client/main.go
+++ client/main.go
@@ -1,67 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	var err error
-	// run the CLI
-	for {
-		switch operation {
-
-		case "Choose operation":
-			if err = chooseOperation(); err != nil {
-				fmt.Println("Error:", err)
-				return
-			}
-
-		case "Get All Questions":
-			if err = runGetAllForm(); err != nil {
-				fmt.Println("error getting all questions from server: ", err)
-				operation = "Choose operation"
-			}
-
-		case "Get Question by ID":
-			if err = RunGetByIdForm(); err != nil {
-				fmt.Println("error getting question from server: ", err)
-				operation = "Choose operation"
-			}
-
-		case "Show single question":
-			if err = runShowSingleForm(); err != nil {
-				fmt.Println("error getting question from server: ", err)
-				operation = "Choose operation"
-			}
-
-		case "Create Question":
-			if err = runCreateQuestionForm(); err != nil {
-				fmt.Println("error creating question: ", err)
-			}
-			operation = "Choose operation"
-
-		case "Update Question":
-			if err := runUpdateForm(); err != nil {
-				fmt.Println("error updating question: ", err)
-			}
-			operation = "Choose operation"
-
-		case "Delete Question":
-			if err = runDeleteForm(); err != nil {
-				fmt.Println("error deleting question:", err)
-			}
-			operation = "Choose operation"
-
-		case "Check Answer":
-			if err = runCheckForm(); err != nil {
-				operation = "Choose operation"
-				fmt.Println("error testing your answer: ", err)
-			}
-		case "Exit":
-			return
-
-		default:
-			fmt.Println("Invalid operation selected.")
-			return
-		}
-	}
+	RunCliForms()
 }
